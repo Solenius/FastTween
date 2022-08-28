90c9147cf8b9507b723e1ff8cd29d6ab8d8122b8
Solenius <>
2022-08-28 23:47:07 +0300

    Added TweenBuffer into correct namespace

--- Assets/Scripts/FastTween/Utils/TweenBuffer.cs
+++ Assets/Scripts/FastTween/Utils/TweenBuffer.cs
@@ -1,38 +1,41 @@
 using System;
 using UnityEngine;
 
-internal class TweenBuffer<T>
+namespace FastTween
 {
-    private T[] m_buffer;
-    private int m_count;
-    private readonly int m_maxGrowth;
-
-    public T[] Buffer => m_buffer;
-    public int Count => m_count;
-
-    public TweenBuffer(int initialSize, int maxGrowth)
+    internal sealed class TweenBuffer<T>
     {
-        m_buffer = new T[initialSize];
-        m_maxGrowth = maxGrowth;
-    }
+        private T[] m_buffer;
+        private int m_count;
+        private readonly int m_maxGrowth;
 
-    public int Add(T item)
-    {
-        if (m_buffer.Length == m_count)
-            Grow();
+        public T[] Buffer => m_buffer;
+        public int Count => m_count;
 
-        m_buffer[m_count] = item;
-        return m_count++;
-    }
+        public TweenBuffer(int initialSize, int maxGrowth)
+        {
+            m_buffer = new T[initialSize];
+            m_maxGrowth = maxGrowth;
+        }
 
-    public void Remove(int index)
-    {
-        m_buffer[index] = m_buffer[--m_count];
-    }
+        public int Add(T item)
+        {
+            if (m_buffer.Length == m_count)
+                Grow();
 
-    private void Grow()
-    {
-        int size = Mathf.Clamp(m_buffer.Length * 2, 16, m_maxGrowth);
-        Array.Resize(ref m_buffer, m_buffer.Length + size);
+            m_buffer[m_count] = item;
+            return m_count++;
+        }
+
+        public void Remove(int index)
+        {
+            m_buffer[index] = m_buffer[--m_count];
+        }
+
+        private void Grow()
+        {
+            int size = Mathf.Clamp(m_buffer.Length * 2, 16, m_maxGrowth);
+            Array.Resize(ref m_buffer, m_buffer.Length + size);
+        }
     }
 }
